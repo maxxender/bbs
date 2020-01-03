7177456c6009c6118595f95a5b194c58d0c6a31f
maxxender <40081426+maxxender@users.noreply.github.com>
2020-01-03 14:13:40 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.bbs.controleur;
+
+import com.bbs.vue.Fenetre;
+import com.bbs.vuefx.FenetreFx;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @return 
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Fenetre f = new Fenetre();
+		
+	}
+
+}
